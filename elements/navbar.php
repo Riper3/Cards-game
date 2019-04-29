ac2ac1c43fa571a5368b4d91ece2324ef6364233
Javier Gonzalez Llamas <lastoner4@gmail.com>
2019-04-29 08:48:43 +0000

    Added the navbar

---
+++ elements/navbar.php
@@ -0,0 +1,9 @@
+<nav>
+<div class="navbar">
+  <div class="nav-element">Home</div>
+  <div class="nav-element">Game</div>
+  <div class="nav-element">Ranking</div>
+  <div class="nav-login">Login</div>
+  <div class="nav-login">Register</div>
+</div>
+</nav>
---
+++ index.php
@@ -0,0 +1,6 @@
+<head>
+  <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+</head>
+<body>
+  <?php require_once "elements/navbar.php"; ?>
+</body>
