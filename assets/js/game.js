32984468836958a57e222395a84c2b0beb09836f
Javier Gonzalez Llamas <lastoner4@gmail.com>
2019-05-02 09:54:39 +0000

    Game clicks done

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,6 +13,7 @@ $(".game-difficulty").click(function(){
     var hard = 1;
   }
 
+  // Cards generator
   var z = 0;
 
   for (var i = 0; i < 40; i++) {
@@ -26,16 +27,29 @@ $(".game-difficulty").click(function(){
     }
   }
 });
+// Game
+var clicksnumber = 0;
+var firstclicked;
 
 $("div").on("click", ".game-card", function(){
+  ++clicksnumber;
   var number = $(this).attr('id').match(/\d+/)[0];
 
   var image = '"'+images[number]+'"';
   $(this).css("background-image", "url("+image+")");
 
   var clicked = $(".game-card[style*='background-image: url("+image+")']");
-  if(clicked.length === 2) {
+  if(clicked.length == 2 && clicksnumber == 2) {
     clicked.css("background-image", "");
     clicked.css('visibility', 'hidden');
+    clicksnumber = 0;
+  }
+  else if(clicksnumber == 2) {
+    clicked.css("background-image", "url('assets/images/card.png')");
+    firstclicked.css("background-image", "url('assets/images/card.png')");
+    clicksnumber = 0;
+  }
+  else if(clicksnumber == 1) {
+    firstclicked = $(this);
   }
 });
