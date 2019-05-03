b14d59b4152a6df31e48eed339ae77c22084ef23
Javier Gonzalez Llamas <lastoner4@gmail.com>
2019-05-03 15:29:23 +0000

    Added frontend check to the register

--- assets/js/fronted.js
+++ assets/js/fronted.js
@@ -29,20 +29,67 @@ $("input").blur(function(){
   $(this).css("color", "black");
 });
 
-// Ajax Register
-
+// Register validation
 $("#submit-register").click(function(){
-  $.ajax({
-      url: 'phpfunctions/registerfunction.php',
-      type: "POST",
-      data: { "username" : $("#new-username").val(),
-              "password" : $("#new-password").val(),
-              "email" : $("#new-email").val()
-            },
-      success: function(reponse) {
-        console.log(reponse);
-      }
-  });
+  $(".error-register").remove();
+
+  var username = $("#new-username");
+  var password = $("#new-password");
+  var email = $("#new-email");
+  var errors = [];
+  var error = 0;
+
+  if(username.val().length == 0) {
+    errors[error] = {"key":"username", "text":"The username is required"};
+    error++;
+  }
+  else if(username.val().length < 4 || username.val().length > 12) {
+    errors[error] = {"key":"username", "text":"The username should have beteween 4 and 12 characteres"};
+    error++;
+  }
+
+  if(password.val().length == 0) {
+    errors[error] = {"key":"password", "text":"The password is required"};
+    error++;
+  }
+  else if(password.val().length <= 6) {
+    errors[error] = {"key":"password", "text":"The password should have at least 6 characters"};
+    error++;
+  }
+
+  var regexemail = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+  if(email.val().length == 0) {
+    errors[error] = {"key":"email", "text":"The email is required"};
+    error++;
+  }
+  else if(!regexemail.test(email.val())) {
+    errors[error] = {"key":"email", "text":"The email is invalid"};
+    error++;
+  }
+
+  for (var i = 0; i < errors.length; i++) {
+    var msg = $("<span class='error-register'></span>").appendTo("body");
+    msg.css("top", $("#new-"+errors[i].key).offset().top + 45);
+    msg.text(errors[i].text);
+    console.log(msg);
+    msg.show();
+  }
+
+  // Ajax register
+  if(error == 0) {
+    $.ajax({
+        url: 'phpfunctions/registerfunction.php',
+        type: "POST",
+        data: { "username" : username.val(),
+                "password" : password.val(),
+                "email" : email.val()
+              },
+        success: function(reponse) {
+          console.log(reponse);
+        }
+    });
+  }
 });
 
 //Game options
@@ -50,7 +97,7 @@ $("#submit-register").click(function(){
 $(".game-time").click(function(){
   $(".game-time").hide();
   $(".game-difficulty").show();
-  
+
   if($(this).text() == "Time trial") {
     $("#time-value").val(2);
   }
